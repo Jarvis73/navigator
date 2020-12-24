e1cc936a95186a702991b980f5b31d0a4991c08c
Jarvis73 <zjw.math@qq.com>
2020-12-24 11:24:42 +0800

    添加百度统计代码

---
+++ js/tools.js
@@ -0,0 +1,8 @@
+// 百度统计分析
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?371f6f100ab5dddfc05196b3c1f10f68";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
