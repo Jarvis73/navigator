61fa73bbb4c1a43c18c130638266d4f393bcf32e
Jarvis73 <zjw.math@qq.com>
2020-12-24 11:22:43 +0800

    添加百度统计代码

---
+++ js/tools.js
@@ -0,0 +1,8 @@
+// 百度统计分析
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b1d411686029ab4ee4ca366d5fd745d8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
